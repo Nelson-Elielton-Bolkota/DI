1790c9253d40f171282af544ae1acf858aa36eac
Nelson <bolkotanelson@gmail.com>
2026-05-18 00:05:58 -0300

    criação de construtor 1

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -8,4 +8,13 @@ public class Produto {
     private double preco;
     private int estoque;
     private CategoriaProduto categoria;
+
+    //criação de construtor que le do banco de dados
+    public Produto(int id, String nome, double preco, int estoque, CategoriaProduto categoria){
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+        this.categoria = categoria;
+    }
 }
