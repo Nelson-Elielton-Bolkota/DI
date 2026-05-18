eae803d6b8915d79a570a182b7462f50269b2bb7
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-05-18 00:16:44 -0300

    Criação da classe item pedido

---
+++ src/model/ItemPedido.java
@@ -0,0 +1,56 @@
+package model;
+package src.main.java.com.gestao.model
+
+public class ItemPedido {
+    private int id;
+    private int idPedido;
+    private Produto produto;
+    private int quantidade;
+    private double precoUnitario;
+
+    // Construtor para leitura do Banco
+
+    public ItemPedido(int id, int idPedido, Produto produto, int quantidade, double precoUnitario){
+        this.id = id;
+        this.idPedido = idPedido;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    // Ao criar um pedido(Sem o id)
+    public ItemPedido(int idPedido, Produto produto, int quantidade){
+        this(0, idPedido, produto, quantidade, produto.getPreco());
+    }
+
+     public int getId() {
+        return id;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public double getSubtotal() {
+        return precoUnitario * quantidade;
+    }
+
+    @Override
+
+    public String toString(){
+       return String.format("  → %s x%d @ R$ %.2f = R$ %.2f",
+                produto.getNome(), quantidade, precoUnitario, getSubtotal());
+}
+}
