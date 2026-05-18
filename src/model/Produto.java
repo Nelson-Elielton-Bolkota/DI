04e53d60deb7a66f0e92e69ef6b920b3f9db4e31
Nelson <bolkotanelson@gmail.com>
2026-05-17 23:59:03 -0300

    criação classe produto

---
+++ src/model/Produto.java
@@ -0,0 +1,11 @@
+package model;
+
+import enums.CategoriaProduto;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private int estoque;
+    private CategoriaProduto categoria;
+}
