29fa2793227dae1ac9fcf5d005ec6e96f1742659
Nelson <bolkotanelson@gmail.com>
2026-05-28 17:57:22 -0300

    criação construtor menuPrincipal

--- src/menu/MenuPrincipal.java
+++ src/menu/MenuPrincipal.java
@@ -1,5 +1,12 @@
 package menu;
 
+import java.util.Scanner;
+
 public class MenuPrincipal {
     
+    private Scanner scanner;
+
+    public MenuPrincipal(Scanner scanner) {
+		this.scanner = scanner;
+	}
 }
