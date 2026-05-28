debccda8a1d8db684fbf1f9d370d24a6ae12968d
Nelson <bolkotanelson@gmail.com>
2026-05-28 18:06:04 -0300

    criação construtor menuProduto

--- src/menu/MenuProduto.java
+++ src/menu/MenuProduto.java
@@ -1,5 +1,15 @@
 package menu;
 
+import java.util.Scanner;
+
 public class MenuProduto {
+    private Scanner scanner;
+
+    public MenuProduto(Scanner scanner) {
+        this.scanner = scanner;
+    }
     
+    public void exibir(){
+        
+    }
 }
