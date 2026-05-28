7484626ddc759fb84854108aff7a724272927418
Nelson <bolkotanelson@gmail.com>
2026-05-28 19:13:07 -0300

    criação metodo salvar na classe produtoDAO e metodo exibir em MenuProduto

--- src/DAO/ProdutoDAO.java
+++ src/DAO/ProdutoDAO.java
@@ -1,5 +1,24 @@
 package DAO;
 
+import enums.*;
+import infra.*;
+import model.Produto;
+import java.sql.*;
+
+
 public class ProdutoDAO {
+    public void salvar(Produto produto) throws SQLException{
+        String sql = "insert into produtos(nome, preco, estoque, categoria) values (?,?,?,?)";
     
+    try (Connection conn = Conexao.conectar();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, produto.getNome());
+            ps.setDouble(2, produto.getPreco());
+            ps.setInt(3, produto.getEstoque());
+            ps.setString(4, produto.getCategoria().name());
+            ps.executeUpdate();
+        
+    } 
+    }
 }
