a4afa552d386bc3ff00cd01fc71275024ee40058
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-05-29 11:22:07 -0300

    finalizando o metodo salvar

--- src/DAO/ItemPedidoDAO.java
+++ src/DAO/ItemPedidoDAO.java
@@ -11,5 +11,12 @@ import model.Produto;
 public class ItemPedidoDAO {
     public void salvar(ItemPedido item_pedido) throws SQLException{
         String sql = "insert into itempedido(quantidade, preco_Unitario) values(?,?)";
+
+        try(Connection conn = Conexao.conectar();
+        PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setInt(1, ItemPedido.getQuantidade());
+            ps.setDouble(2, ItemPedido.getPrecoUnitario());
+        }
+
     }
 }
