38a0d07335f86c8defbc65dd7038ec74b651d40b
João Vitor Kutianski <engs-joaokutianski@camporeal.edu.br>
2026-05-30 12:27:45 -0300

    Importação das bibliotecas para o menuPedido

--- src/menu/MenuPedido.java
+++ src/menu/MenuPedido.java
@@ -1,4 +1,17 @@
 package menu;
+
+import DAO.PedidoDAO;
+import DAO.ProdutoDAO;
+import DAO.ClienteDAO;
+import infra.EstoqueInsuficienteException;
+
+import model.Cliente;
+import model.Produto;
+import model.ItemPedido;
+import model.Pedido;
+
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class MenuPedido {
