b6669b30af68cb18fa2c185b076b4bd509f889d6
Luis <engs-luiskomar@camporeal.edu.br>
2026-06-01 20:09:43 -0300

    imports do munu clientes

--- src/menu/MenuCliente.java
+++ src/menu/MenuCliente.java
@@ -6,12 +6,14 @@ import java.util.Scanner;
 
 public class MenuCliente {
     private Scanner scanner;
+    private ClienteDAO clienteDAO;
 
     public MenuCliente(Scanner scanner) {
         this.scanner = scanner;
+        this.clienteDAO = new ClienteDAO();
     }
     
     public void exibir(){
-        
+        int opcao;
     }
 }
