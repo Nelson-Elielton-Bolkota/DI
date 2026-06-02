d3712b53832e2f5444fab1cc9f4de22a1133ce68
Luis <engs-luiskomar@camporeal.edu.br>
2026-06-02 09:06:35 -0300

    criação metodo listar

--- src/menu/MenuCliente.java
+++ src/menu/MenuCliente.java
@@ -4,6 +4,7 @@ import DAO.ClienteDAO;
 import model.Cliente;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class MenuCliente {
@@ -55,12 +56,21 @@ public class MenuCliente {
         }
     }
 
+    private void listar() {
+        try {
+            List<Cliente> clientes = clienteDAO.buscarTodos();
+            if (clientes.isEmpty()) {
+                System.out.println("Nenhum cliente cadastrado.");
+            } else {
+                clientes.forEach(System.out::println);
+            }
+
     private void buscar() {
         System.out.print("ID do cliente: ");
         int id = lerInteiro();
     
         try {
-            Cliente cliente = ClienteDAO.buscarPorId(id);
+            Cliente cliente = clienteDAO.buscarPorId(id);
             if (cliente != null) {
                 System.out.println(cliente);
             } else {
