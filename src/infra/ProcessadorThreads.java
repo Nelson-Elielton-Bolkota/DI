310f757ea0154ce89bafb85b44be8eada8bfa1f4
Nelson <bolkotanelson@gmail.com>
2026-06-01 21:20:10 -0300

    criação da classe ProcessadorThreads

---
+++ src/infra/ProcessadorThreads.java
@@ -0,0 +1,11 @@
+package infra;
+
+public class ProcessadorThreads implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
