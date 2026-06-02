9aebbcc77e142c24141fb516f1d6df26baa568b8
Luis <engs-luiskomar@camporeal.edu.br>
2026-06-01 21:22:02 -0300

    Prints do exibir

--- src/menu/MenuCliente.java
+++ src/menu/MenuCliente.java
@@ -15,5 +15,19 @@ public class MenuCliente {
     
     public void exibir(){
         int opcao;
+        do {
+            System.out.println("\n=== MENU CLIENTE ===");
+            System.out.println("1 - Cadastrar cliente");
+            System.out.println("2 - Listar todos");
+            System.out.println("3 - Buscar por ID");
+            System.out.println("4 - Atualizar cliente");
+            System.out.println("5 - Deletar cliente");
+            System.out.println("0 - Voltar");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = lerInteiro();
+
+            
+        }
     }
 }
