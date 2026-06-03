a400647c78111d1ed461abc0d37208e9894966e7
Nelson <bolkotanelson@gmail.com>
2026-06-03 00:30:24 -0300

    metodo parar em processadorThreads

--- src/infra/ProcessadorThreads.java
+++ src/infra/ProcessadorThreads.java
@@ -2,10 +2,15 @@ package infra;
 
 public class ProcessadorThreads implements Runnable{
 
+    public volatile boolean rodando = true;
+
     @Override
     public void run() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    public void parar(){
+        this.rodando = false;
     }
 
 }
