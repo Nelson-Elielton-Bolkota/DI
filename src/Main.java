fb5f4f3f7540b008fbd25a8314749e5043305545
Nelson <bolkotanelson@gmail.com>
2026-06-03 15:30:12 -0300

    metodo processarCiclo em processadorThreads

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 import menu.MenuPrincipal;
-
+import infra.ProcessadorThreads;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner; 
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in, "CP850");
+
+        ProcessadorThreads processador = new ProcessadorThreads();
+
+        Thread threadProcessador = new Thread(processador);
+        threadProcessador.start();
+
         MenuPrincipal menu = new MenuPrincipal(scanner);
         menu.iniciar();
+
+        System.out.println("Encerrando serviços em segundo plano...");
+        processador.parar();
+
         scanner.close();
+        System.out.println("Sistema encerrado com sucesso.");
     }
 }
