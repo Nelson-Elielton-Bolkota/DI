049b06e958904456506cc15ad64d529284b64a5c
Nelson <bolkotanelson@gmail.com>
2026-06-03 00:34:15 -0300

    criação metodo processarCiclo em processadorThreads

--- src/infra/ProcessadorThreads.java
+++ src/infra/ProcessadorThreads.java
@@ -1,5 +1,8 @@
 package infra;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ProcessadorThreads implements Runnable{
 
     public volatile boolean rodando = true;
@@ -9,6 +12,15 @@ public class ProcessadorThreads implements Runnable{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
+
+    public void processarCiclo(){
+        try(Connection conn = Conexao.conectar()){
+
+        }catch(SQLException e){
+            System.out.println("[THREAD] erro no banco de dados durando o processo "+ e.getMessage());
+        }
+    }
+
     public void parar(){
         this.rodando = false;
     }
